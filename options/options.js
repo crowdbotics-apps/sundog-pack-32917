0159020ab861941ffaf596bcad41d4a537b56b4c
Crowdbotics <team@crowdbotics.com>
2022-01-17 22:28:57 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "sundog_pack_32917",
+  url: "https://sundog_pack_32917.botics.co",
+  api: "https://sundog_pack_32917.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
